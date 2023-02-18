adc9a1796b340dd1ca94c9927b2fc5bdc1b2280d
Nazik <nazikbaktygulova77@gmail.com>
2023-02-18 13:46:23 +0600

    Написан код для задачи №4 в папке hw_4

---
+++ hw_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while(index <= N) {
+    if(index % 2 == 1) {
+        index++;
+    }
+    else {
+        Console.Write(index + " ");
+        index ++;
+    }
+}
