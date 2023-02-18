cc1b35b0404c3ce767a530ad2d5c26a2413b9652
Nazik <nazikbaktygulova77@gmail.com>
2023-02-18 13:04:35 +0600

    Написан код для задачи №1 в папке hw_1

---
+++ hw_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает, 
+// какое число больше, а какое меньше
+
+int a = 5;
+int b = 9;
+
+int max;
+int min;
+
+if(a > b) {
+    max = a;
+    min = b;
+}
+else {
+    max = b;
+    min = a;
+}
+
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
