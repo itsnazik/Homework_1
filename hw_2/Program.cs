c3ddbe60f855e1a065a667e93019742f8fcb1fb0
Nazik <nazikbaktygulova77@gmail.com>
2023-02-18 13:12:53 +0600

    Написан код для задачи №2 в папке hw_2

---
+++ hw_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и выдaет максимальное из этих чисел
+
+int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+
+if(a > max) {
+    max = a;
+}
+else if(b > max) {
+    max = b;
+}
+else if(c > max) {
+    max = c;
+}
+
+Console.WriteLine("max = " + max);
